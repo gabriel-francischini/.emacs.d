2c2e0173d61babb1261a8fc6e39ac3b1b0bdb358
Gabriel Francischini <empresagabriel@gmail.com>
2018-04-11 23:08:17 -0300

    Removing old melpa mirror with broken packages

--- init.el
+++ init.el
@@ -1,8 +1,5 @@
-
 ;; Package initialization
 (require 'package)
-(add-to-list 'package-archives
-         '("melpa" . "http://melpa.org/packages/") t)
 
 (add-to-list 'package-archives
       '(("gnu" . "http://elpa.gnu.org/packages/")
